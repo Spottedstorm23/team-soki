3f06691deb3cc794e7213a097380def6faf37fe1
Cora Stokowsky <73465904+Spottedstorm23@users.noreply.github.com>
2022-04-06 18:04:27 +0200

    Task: pom.xml und .gitignore aktualisiert, MANIFEST.MF angelegt

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module TeamSoki.Textadventure {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
 
     // https://www.tutorialguruji.com/java/illegalaccessexception-cannot-access-class-c-in-module-m-because-module-m-does-not-export-c-to-module-n/
     opens soki.textadventure.controller to javafx.fxml;
+    exports soki.textadventure.controller;
 
     // TODO: Still needed?
     opens soki.textadventure to javafx.fxml;
