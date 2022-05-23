20f05042b7734c55f389df2bbbb0175c804828a7
Cora Vanessa Stokowsky <73465904+Spottedstorm23@users.noreply.github.com>
2022-05-23 16:56:31 +0200

    4 speicherfunktion (#10)
    * Erster Entwurf JSON Reader implementiert
    
    * Funktionen zum:
    
    1. Lesen/Schreiben von JSON
    2. spezifischen Text aus JSON bekommen
    3. Fortschritt des Spiels in JSON abspeichern
    
    implementiert
    
    * Funktionen für die Speicherverwaltung nocheinmal neugeschrieben (verbessert)
    
    * Funktionen für die PlayerData von Mehrspieler auf Einzelspieler heruntergekürzt. "FileController_old" enthält die alten Funktionen für Mehrspieler.
    
    * Weitere Funktionen für die PlayerData hinzugefügt sowie bestehende Funktionen geänder, 2 Funktionen für text.json, sowie ein Beispiel-Text.json hinzugefügt.
    
    Co-authored-by: Luzzzo <lukas.keinert@web.de>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module TeamSoki.Textadventure {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.controlsfx.controls;
+    requires json.simple;
     requires java.desktop;
 
     // https://www.tutorialguruji.com/java/illegalaccessexception-cannot-access-class-c-in-module-m-because-module-m-does-not-export-c-to-module-n/
